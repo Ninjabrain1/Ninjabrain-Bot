f3c81f52bf68621827af4d3844a1a89185bd1e9c
Filip Ryblad <firybl@gmail.com>
2022-05-20 16:54:50 +0200

    changed version string

--- src/ninjabrainbot/Main.java
+++ src/ninjabrainbot/Main.java
@@ -11,7 +11,7 @@ import ninjabrainbot.util.Profiler;
 
 public class Main {
 
-	public static final String VERSION = "1.2.4";
+	public static final String VERSION = "1.3.0";
 	public static NinjabrainBotPreferences preferences;
 
 	public static void main(String[] args) {
