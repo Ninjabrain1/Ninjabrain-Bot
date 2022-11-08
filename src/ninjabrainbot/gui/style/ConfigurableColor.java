f8891e93d5d6eaa18ad1b37b8861f7c5d7f85952
Filip Ryblad <firybl@gmail.com>
2022-11-08 20:51:03 +0100

    serialize theme

--- src/ninjabrainbot/gui/style/ConfigurableColor.java
+++ src/ninjabrainbot/gui/style/ConfigurableColor.java
@@ -5,10 +5,13 @@ public class ConfigurableColor {
 	public final WrappedColor color;
 	
 	public final String name;
+
+	public final String uid;
 	
-	public ConfigurableColor(WrappedColor color, String name) {
+	public ConfigurableColor(WrappedColor color, String name, String uid) {
 		this.color = color;
 		this.name = name;
+		this.uid = uid;
 	}
 	
 }
--- src/ninjabrainbot/gui/style/CustomTheme.java
+++ src/ninjabrainbot/gui/style/CustomTheme.java
@@ -11,24 +11,46 @@ public class CustomTheme extends Theme {
 		super("Custom");
 		configurableColors = new ArrayList<ConfigurableColor>();
 
-		COLOR_STRONGEST = createColor(Color.decode("#1C1C27"), "Title bar");
-		COLOR_STRONG = createColor(Color.decode("#252538"), "Header background");
-		COLOR_SLIGHTLY_WEAK = createColor(Color.decode("#2B2D44"), "Result background");
-		COLOR_NEUTRAL = createColor(Color.decode("#28293D"), "Throws background");
-		COLOR_DIVIDER = createColor(Color.decode("#212130"), "Dividers");
-		COLOR_DIVIDER_DARK = createColor(Color.decode("#1C1C27"), "Header Dividers");
+		COLOR_STRONGEST = createColor(Color.decode("#1C1C27"), "Title bar", "a");
+		COLOR_STRONG = createColor(Color.decode("#252538"), "Header background", "b");
+		COLOR_SLIGHTLY_WEAK = createColor(Color.decode("#2B2D44"), "Result background", "c");
+		COLOR_NEUTRAL = createColor(Color.decode("#28293D"), "Throws background", "d");
+		COLOR_DIVIDER = createColor(Color.decode("#212130"), "Dividers", "e");
+		COLOR_DIVIDER_DARK = createColor(Color.decode("#1C1C27"), "Header Dividers", "f");
+		COLOR_EXIT_BUTTON_HOVER = createColor(Color.decode("#F04747"), "Exit button hover", "g");
+		TEXT_COLOR_STRONG = createColor(Color.WHITE, "Text", "h");
+		TEXT_COLOR_SLIGHTLY_STRONG = createColor(Color.decode("#E5E5E5"), "Divine text", "i");
+		TEXT_COLOR_WEAK = createColor(Color.GRAY, "Version text", "j");
+		TEXT_COLOR_NEUTRAL = createColor(Color.LIGHT_GRAY, "Throws text", "k");
+		COLOR_POSITIVE = createColor(Color.decode("#75CC6C"), "Subpixel +", "l");
+		COLOR_NEGATIVE = createColor(Color.decode("#CC6E72"), "Subpixel -", "m");
+		
 		COLOR_SLIGHTLY_STRONG = createColor(Color.decode("#27273D"), COLOR_STRONG);
-		COLOR_EXIT_BUTTON_HOVER = createColor(Color.decode("#F04747"), "Exit button hover");
-		TEXT_COLOR_STRONG = createColor(Color.WHITE, "Text");
-		TEXT_COLOR_SLIGHTLY_STRONG = createColor(Color.decode("#E5E5E5"), "Divine text");
-		TEXT_COLOR_WEAK = createColor(Color.GRAY, "Version text");
-		TEXT_COLOR_NEUTRAL = createColor(Color.LIGHT_GRAY, "Throws text");
 		COLOR_SATURATED = createColor(Color.decode("#57EBA3"));
-		COLOR_POSITIVE = createColor(Color.decode("#75CC6C"), "Subpixel +");
-		COLOR_NEGATIVE = createColor(Color.decode("#CC6E72"), "Subpixel -");
 
 		CERTAINTY_COLOR_MAP = createColorMap(new ColorMap(Color.RED, Color.YELLOW, Color.decode("#00CE29")));
 		BLACK_ICONS = createBoolean(false);
+	}
+	
+	public void setFromTheme(Theme theme) {
+		COLOR_STRONGEST.set(theme.COLOR_STRONGEST);
+		COLOR_STRONG.set(theme.COLOR_STRONG);
+		COLOR_SLIGHTLY_WEAK.set(theme.COLOR_SLIGHTLY_WEAK);
+		COLOR_NEUTRAL.set(theme.COLOR_NEUTRAL);
+		COLOR_DIVIDER.set(theme.COLOR_DIVIDER);
+		COLOR_DIVIDER_DARK.set(theme.COLOR_DIVIDER_DARK);
+		COLOR_SLIGHTLY_STRONG.set(theme.COLOR_SLIGHTLY_STRONG);
+		COLOR_EXIT_BUTTON_HOVER.set(theme.COLOR_EXIT_BUTTON_HOVER);
+		TEXT_COLOR_STRONG.set(theme.TEXT_COLOR_STRONG);
+		TEXT_COLOR_SLIGHTLY_STRONG.set(theme.TEXT_COLOR_SLIGHTLY_STRONG);
+		TEXT_COLOR_WEAK.set(theme.TEXT_COLOR_WEAK);
+		TEXT_COLOR_NEUTRAL.set(theme.TEXT_COLOR_NEUTRAL);
+		COLOR_SATURATED.set(theme.COLOR_SATURATED);
+		COLOR_POSITIVE.set(theme.COLOR_POSITIVE);
+		COLOR_NEGATIVE.set(theme.COLOR_NEGATIVE);
+
+		BLACK_ICONS.set(theme.BLACK_ICONS);
+		CERTAINTY_COLOR_MAP.set(theme.CERTAINTY_COLOR_MAP);
 	}
 
 	public ArrayList<ConfigurableColor> getConfigurableColors() {
@@ -52,9 +74,11 @@ public class CustomTheme extends Theme {
 		return i;
 	}
 
-	protected WrappedColor createColor(Color color, String name) {
+	protected WrappedColor createColor(Color color, String name, String uid) {
+		assert uid.length() == 1;
+		assert !configurableColors.stream().anyMatch(cc -> cc.uid == uid);
 		WrappedColor wc = super.createColor(color);
-		configurableColors.add(new ConfigurableColor(wc, name));
+		configurableColors.add(new ConfigurableColor(wc, name, uid));
 		return wc;
 	}
 
