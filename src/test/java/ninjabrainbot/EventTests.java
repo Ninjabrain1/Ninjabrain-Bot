cce73ceaef06d7ddc93858471a7c8c88922bf961
Filip Ryblad <firybl@gmail.com>
2022-12-08 23:47:41 +0100

    updated tests to use JUnit

--- src/test/java/ninjabrainbot/EventTests.java
+++ src/test/java/ninjabrainbot/EventTests.java
@@ -3,6 +3,7 @@ package ninjabrainbot;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import junit.framework.TestCase;
@@ -16,11 +17,12 @@ import ninjabrainbot.event.ObservableField;
 
 public class EventTests extends TestCase {
 
+	IModificationLock lock;
 	IModifiableSet<IThrow> set;
 
-	@Override
-	protected void setUp() throws Exception {
-		IModificationLock lock = new AlwaysUnlocked();
+	@BeforeEach
+	public void setUp() {
+		lock = new AlwaysUnlocked();
 		set = new ModifiableSet<IThrow>();
 
 		set.add(new Throw(313, 277, 90, -31, false, lock));
@@ -31,33 +33,26 @@ public class EventTests extends TestCase {
 
 	@Test
 	public void modifiableSet_setFromList_shouldntRaiseChangeEvent() {
-
 		List<IThrow> list = set.toList();
 
 		ObservableField<Boolean> whenModifiedWasRaised = new ObservableField<Boolean>(false);
 
 		set.whenModified().subscribe(__ -> whenModifiedWasRaised.set(true));
 		set.setFromList(list);
-		assert whenModifiedWasRaised.get() == false;
+		Assertions.assertFalse(whenModifiedWasRaised.get());
 
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised.set(true), 0);
 		set.setFromList(list);
-		assert whenModifiedWasRaised.get() == false;
+		Assertions.assertFalse(whenModifiedWasRaised.get());
 
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised.set(true), 4);
 		set.setFromList(list);
 		Assertions.assertFalse(whenModifiedWasRaised.get());
 	}
 
+	@Test
 	public void modifiableSet_setFromList_shouldRaiseChangeEvent() {
 		// Arrange
-		IModificationLock lock = new AlwaysUnlocked();
-		IModifiableSet<IThrow> set = new ModifiableSet<IThrow>();
-		set.add(new Throw(313, 277, 90, -31, false, lock));
-		set.add(new Throw(4, 2660, 30, -31, false, lock));
-		set.add(new Throw(670, 273, 20, -31, false, lock));
-		set.add(new Throw(1200, 243, -90, -31, false, lock));
-
 		List<IThrow> list = set.toList();
 		list.remove(2);
 
@@ -77,72 +72,72 @@ public class EventTests extends TestCase {
 		set.setFromList(list);
 
 		// Assert
-		assert whenModifiedWasRaised.get() == true;
-		assert whenModifiedWasRaised_idx0.get() == false;
-		assert whenModifiedWasRaised_idx1.get() == false;
-		assert whenModifiedWasRaised_idx2.get() == true;
-		assert whenModifiedWasRaised_idx3.get() == true;
+		Assertions.assertTrue(whenModifiedWasRaised.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx0.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx1.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx2.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx3.get());
 	}
 
+	@Test
 	public void modifiableSet_add_shouldRaiseChangeEvent() {
 		// Arrange
-		IModificationLock lock = new AlwaysUnlocked();
-		IModifiableSet<IThrow> set = new ModifiableSet<IThrow>();
-		set.add(new Throw(313, 277, 90, -31, false, lock));
-		set.add(new Throw(4, 2660, 30, -31, false, lock));
-
 		ObservableField<Boolean> whenModifiedWasRaised = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx0 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx1 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx2 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx3 = new ObservableField<Boolean>(false);
+		ObservableField<Boolean> whenModifiedWasRaised_idx4 = new ObservableField<Boolean>(false);
+		ObservableField<Boolean> whenModifiedWasRaised_idx5 = new ObservableField<Boolean>(false);
 
 		set.whenModified().subscribe(__ -> whenModifiedWasRaised.set(true));
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx0.set(true), 0);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx1.set(true), 1);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx2.set(true), 2);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx3.set(true), 3);
+		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx4.set(true), 4);
+		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx5.set(true), 5);
 
 		// Act
 		set.add(new Throw(670, 273, 20, -31, false, lock));
 
 		// Assert
-		assert whenModifiedWasRaised.get() == true;
-		assert whenModifiedWasRaised_idx0.get() == false;
-		assert whenModifiedWasRaised_idx1.get() == false;
-		assert whenModifiedWasRaised_idx2.get() == true;
-		assert whenModifiedWasRaised_idx3.get() == false;
+		Assertions.assertTrue(whenModifiedWasRaised.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx0.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx1.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx2.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx3.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx4.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx5.get());
 	}
 
+	@Test
 	public void modifiableSet_remove_shouldRaiseChangeEvent() {
 		// Arrange
-		IModificationLock lock = new AlwaysUnlocked();
-		IModifiableSet<IThrow> set = new ModifiableSet<IThrow>();
-		set.add(new Throw(313, 277, 90, -31, false, lock));
-		set.add(new Throw(4, 2660, 30, -31, false, lock));
-		set.add(new Throw(670, 273, 20, -31, false, lock));
-
 		ObservableField<Boolean> whenModifiedWasRaised = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx0 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx1 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx2 = new ObservableField<Boolean>(false);
 		ObservableField<Boolean> whenModifiedWasRaised_idx3 = new ObservableField<Boolean>(false);
+		ObservableField<Boolean> whenModifiedWasRaised_idx4 = new ObservableField<Boolean>(false);
 
 		set.whenModified().subscribe(__ -> whenModifiedWasRaised.set(true));
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx0.set(true), 0);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx1.set(true), 1);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx2.set(true), 2);
 		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx3.set(true), 3);
+		set.whenElementAtIndexModified().subscribe(__ -> whenModifiedWasRaised_idx4.set(true), 4);
 
 		// Act
 		set.remove(set.get(1));
 
 		// Assert
-		assert whenModifiedWasRaised.get() == true;
-		assert whenModifiedWasRaised_idx0.get() == false;
-		assert whenModifiedWasRaised_idx1.get() == true;
-		assert whenModifiedWasRaised_idx2.get() == true;
-		assert whenModifiedWasRaised_idx3.get() == false;
+		Assertions.assertTrue(whenModifiedWasRaised.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx0.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx1.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx2.get());
+		Assertions.assertTrue(whenModifiedWasRaised_idx3.get());
+		Assertions.assertFalse(whenModifiedWasRaised_idx4.get());
 	}
 
 }
