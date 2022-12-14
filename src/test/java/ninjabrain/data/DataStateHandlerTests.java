4cdaaa4dcfe305302f30d348db1ab0bd056f1c27
Filip Ryblad <firybl@gmail.com>
2022-12-15 00:27:01 +0100

    Fixed imports

--- src/test/java/ninjabrain/data/DataStateHandlerTests.java
+++ src/test/java/ninjabrain/data/DataStateHandlerTests.java
@@ -1,8 +1,8 @@
 package ninjabrain.data;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
