05c068c4eda7485cebc24498a285b7133b702e15
Filip Ryblad <firybl@gmail.com>
2023-03-26 16:56:19 +0200

    reformat

--- src/test/java/ninjabrainbot/data/DataStateHandlerTests.java
+++ src/test/java/ninjabrainbot/data/DataStateHandlerTests.java
@@ -13,7 +13,9 @@ import ninjabrainbot.util.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataStateHandlerTests {
 
