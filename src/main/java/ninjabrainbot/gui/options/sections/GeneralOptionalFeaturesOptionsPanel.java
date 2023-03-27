245dbc5536ba569489d68d04472ef17c4b59dd53
Filip Ryblad <firybl@gmail.com>
2023-03-27 22:12:48 +0200

    Add settings for info messages

--- src/main/java/ninjabrainbot/gui/options/sections/GeneralOptionalFeaturesOptionsPanel.java
+++ src/main/java/ninjabrainbot/gui/options/sections/GeneralOptionalFeaturesOptionsPanel.java
@@ -2,6 +2,7 @@ package ninjabrainbot.gui.options.sections;
 
 import javax.swing.border.EmptyBorder;
 
+import ninjabrainbot.gui.components.layout.TitledDivider;
 import ninjabrainbot.gui.frames.OptionsFrame;
 import ninjabrainbot.gui.components.preferences.CheckboxPanel;
 import ninjabrainbot.gui.components.layout.StackPanel;
@@ -9,7 +10,6 @@ import ninjabrainbot.gui.style.StyleManager;
 import ninjabrainbot.io.preferences.NinjabrainBotPreferences;
 import ninjabrainbot.util.I18n;
 
-@SuppressWarnings("serial")
 public class GeneralOptionalFeaturesOptionsPanel extends StackPanel {
 
 	public GeneralOptionalFeaturesOptionsPanel(StyleManager styleManager, NinjabrainBotPreferences preferences) {
@@ -17,6 +17,11 @@ public class GeneralOptionalFeaturesOptionsPanel extends StackPanel {
 		setBorder(new EmptyBorder(OptionsFrame.PADDING, OptionsFrame.PADDING, OptionsFrame.PADDING, OptionsFrame.PADDING));
 
 		add(new CheckboxPanel(styleManager, I18n.get("settings.show_angle_updates"), preferences.showAngleUpdates));
+		add(new TitledDivider(styleManager,"Information messages"));
+		add(new CheckboxPanel(styleManager, I18n.get("settings.information.enable_mismeasure_warning"), preferences.informationMismeasureEnabled));
+		add(new CheckboxPanel(styleManager, I18n.get("settings.information.enable_portal_linking_warning"), preferences.informationPortalLinkingEnabled));
+		add(new CheckboxPanel(styleManager, I18n.get("settings.information.enable_combined_certainty_information"), preferences.informationCombinedCertaintyEnabled));
+		add(new CheckboxPanel(styleManager, I18n.get("settings.information.enable_direction_help_information"), preferences.informationDirectionHelpEnabled));
 	}
 
 }
