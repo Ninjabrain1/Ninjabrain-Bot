1515bcc8bd79df9751609df1698174587f70675e
Filip Ryblad <firybl@gmail.com>
2023-04-11 20:24:26 +0200

    Remove unused classes

--- src/main/java/ninjabrainbot/event/IModifiableSet.java
+++
@@ -1,7 +0,0 @@
-package ninjabrainbot.event;
-
-public interface IModifiableSet<T extends IModifiable<?>> extends IModifiable<IModifiableSet<T>>, IReadOnlyList<T> {
-
-	IMultiSubscribable<T> whenElementAtIndexModified();
-
-}
