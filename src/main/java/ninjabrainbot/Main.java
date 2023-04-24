ba86548031a47f1c433683122ab0f3f447e31698
Filip Ryblad <firybl@gmail.com>
2023-04-24 23:51:57 +0200

    Update version tag

--- src/main/java/ninjabrainbot/Main.java
+++ src/main/java/ninjabrainbot/Main.java
@@ -15,7 +15,7 @@ import ninjabrainbot.util.Profiler;
 
 public class Main {
 
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.4.1";
 
 	public static void main(String[] args) {
 		System.setProperty("sun.java2d.opengl", "true");
