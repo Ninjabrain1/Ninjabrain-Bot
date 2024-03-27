2baea101986058cdbc3c187a0f090c19a4511a64
Filip Ryblad <firybl@gmail.com>
2024-03-27 20:52:45 +0100

    Disable hardware acceleration

--- src/main/java/ninjabrainbot/Main.java
+++ src/main/java/ninjabrainbot/Main.java
@@ -19,9 +19,7 @@ public class Main {
 	public static final String VERSION = "1.4.2";
 
 	public static void main(String[] args) {
-		Optional<String> sessionType = Optional.ofNullable(System.getenv("XDG_SESSION_TYPE")); // Hardware acceleration isn't supported when running java on wayland so we turn it off
-		System.setProperty("sun.java2d.opengl", sessionType.orElse("").equals("wayland") ? "false" : "True");
-		boolean isSplashScreenDisabled = sessionType.isPresent();
+		boolean isSplashScreenDisabled = Optional.ofNullable(System.getenv("XDG_SESSION_TYPE")).isPresent(); // Splash screen does not work for linux (wayland) so we turn it off
 		Progress.init(new Splash(isSplashScreenDisabled));
 		Progress.setTask("Loading language", 0.02f);
 		Profiler.start("Initialize language");
