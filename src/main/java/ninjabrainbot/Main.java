920bd5aad83550006a29c3d4ef05edd9879ffc1a
Filip Ryblad <firybl@gmail.com>
2024-03-27 20:52:51 +0100

    Version string 1.4.3

--- src/main/java/ninjabrainbot/Main.java
+++ src/main/java/ninjabrainbot/Main.java
@@ -16,7 +16,7 @@ import ninjabrainbot.util.Profiler;
 
 public class Main {
 
-	public static final String VERSION = "1.4.2";
+	public static final String VERSION = "1.4.3";
 
 	public static void main(String[] args) {
 		boolean isSplashScreenDisabled = Optional.ofNullable(System.getenv("XDG_SESSION_TYPE")).isPresent(); // Splash screen does not work for linux (wayland) so we turn it off
