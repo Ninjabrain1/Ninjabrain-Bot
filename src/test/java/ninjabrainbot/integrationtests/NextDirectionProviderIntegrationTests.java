a4a5a69db32d680e8203ff352168b536bd3ae9b4
Filip Ryblad <firybl@gmail.com>
2024-05-22 22:21:16 +0200

    fix indent

--- src/test/java/ninjabrainbot/integrationtests/NextDirectionProviderIntegrationTests.java
+++ src/test/java/ninjabrainbot/integrationtests/NextDirectionProviderIntegrationTests.java
@@ -17,69 +17,69 @@ import org.junit.jupiter.api.Test;
 
 public class NextDirectionProviderIntegrationTests {
 
-    @Test
-    void expectedCertaintyOnNextThrowIsAtLeastAsGoodAsPromised() {
-        Logger.enabled = false;
-        // Arrange
-        Random chunkRandom = new Random(111);
-        IntegrationTestBuilder testBuilder = new IntegrationTestBuilder().withProSettings();
-        testBuilder.preferences.informationDirectionHelpEnabled.set(true);
-        RandomPlayerPositionProvider playerPositionProvider = new RandomPlayerPositionProvider(123);
-        NextThrowDirectionInformationProvider infoMessage = new NextThrowDirectionInformationProvider(testBuilder.dataState, testBuilder.environmentState, testBuilder.preferences);
+	@Test
+	void expectedCertaintyOnNextThrowIsAtLeastAsGoodAsPromised() {
+		Logger.enabled = false;
+		// Arrange
+		Random chunkRandom = new Random(111);
+		IntegrationTestBuilder testBuilder = new IntegrationTestBuilder().withProSettings();
+		testBuilder.preferences.informationDirectionHelpEnabled.set(true);
+		RandomPlayerPositionProvider playerPositionProvider = new RandomPlayerPositionProvider(123);
+		NextThrowDirectionInformationProvider infoMessage = new NextThrowDirectionInformationProvider(testBuilder.dataState, testBuilder.environmentState, testBuilder.preferences);
 
-        // Act
-        double totalProbability = 0;
-        int samples = 0;
-        for (int i = 0; i < 100; i++) {
-            testBuilder.resetCalculator();
-            IDetailedPlayerPosition firstPlayerPosition = playerPositionProvider.nextPlayerPositionFirstRing();
-            testBuilder.inputDetailedPlayerPosition(firstPlayerPosition);
-            if (infoMessage.get() == null)
-                continue;
-            Chunk randomStrongholdChunk = sampleRandomChunk(chunkRandom, testBuilder.dataState.calculatorResult().get());
-            testBuilder.inputDetailedPlayerPosition(getPlayerPositionForNextThrow(firstPlayerPosition, randomStrongholdChunk, infoMessage.get().message));
-            List<Chunk> topChunks = testBuilder.dataState.calculatorResult().get().getTopChunks();
-            double eyeSpyProbability = topChunks.get(0).weight + (topChunks.get(0).isNeighboring(topChunks.get(1)) ? topChunks.get(1).weight : 0);
-            totalProbability += eyeSpyProbability;
-            samples++;
-        }
-        System.out.println(totalProbability / samples + ", " + samples);
+		// Act
+		double totalProbability = 0;
+		int samples = 0;
+		for (int i = 0; i < 100; i++) {
+			testBuilder.resetCalculator();
+			IDetailedPlayerPosition firstPlayerPosition = playerPositionProvider.nextPlayerPositionFirstRing();
+			testBuilder.inputDetailedPlayerPosition(firstPlayerPosition);
+			if (infoMessage.get() == null)
+				continue;
+			Chunk randomStrongholdChunk = sampleRandomChunk(chunkRandom, testBuilder.dataState.calculatorResult().get());
+			testBuilder.inputDetailedPlayerPosition(getPlayerPositionForNextThrow(firstPlayerPosition, randomStrongholdChunk, infoMessage.get().message));
+			List<Chunk> topChunks = testBuilder.dataState.calculatorResult().get().getTopChunks();
+			double eyeSpyProbability = topChunks.get(0).weight + (topChunks.get(0).isNeighboring(topChunks.get(1)) ? topChunks.get(1).weight : 0);
+			totalProbability += eyeSpyProbability;
+			samples++;
+		}
+		System.out.println(totalProbability / samples + ", " + samples);
 
-        Assert.isTrue(totalProbability / samples > 0.95, "Average certainty after second throw did not exceed 95%: " + totalProbability / samples);
+		Assert.isTrue(totalProbability / samples > 0.95, "Average certainty after second throw did not exceed 95%: " + totalProbability / samples);
 
-        Logger.enabled = true;
-    }
+		Logger.enabled = true;
+	}
 
-    private Chunk sampleRandomChunk(Random random, ICalculatorResult calculatorResult) {
-        List<Chunk> chunks = calculatorResult.getTopChunks();
-        double sample = random.nextDouble();
-        double cumProb = 0;
-        for (Chunk chunk : chunks) {
-            cumProb += chunk.weight;
-            if (cumProb > sample)
-                return chunk;
-        }
-        return null;
-    }
+	private Chunk sampleRandomChunk(Random random, ICalculatorResult calculatorResult) {
+		List<Chunk> chunks = calculatorResult.getTopChunks();
+		double sample = random.nextDouble();
+		double cumProb = 0;
+		for (Chunk chunk : chunks) {
+			cumProb += chunk.weight;
+			if (cumProb > sample)
+				return chunk;
+		}
+		return null;
+	}
 
-    private IDetailedPlayerPosition getPlayerPositionForNextThrow(IDetailedPlayerPosition playerPosition, Chunk stronghold, String message) {
-        String[] words = message.split(" ");
-        int leftDistance = getFirstInt(words);
-        double leftAngle = playerPosition.horizontalAngle() / 180.0 * Math.PI * 2 - 0.5 * Math.PI;
-        double x = playerPosition.xInOverworld() + Coords.getX(leftDistance, leftAngle);
-        double z = playerPosition.zInOverworld() + Coords.getZ(leftDistance, leftAngle);
-        double horizontalAngle = Coords.getPhi(stronghold.eightEightX() - x, stronghold.eightEightZ() - z) / Math.PI * 180.0;
-        return new DetailedPlayerPosition(x, 80, z, horizontalAngle, -31, false);
-    }
+	private IDetailedPlayerPosition getPlayerPositionForNextThrow(IDetailedPlayerPosition playerPosition, Chunk stronghold, String message) {
+		String[] words = message.split(" ");
+		int leftDistance = getFirstInt(words);
+		double leftAngle = playerPosition.horizontalAngle() / 180.0 * Math.PI * 2 - 0.5 * Math.PI;
+		double x = playerPosition.xInOverworld() + Coords.getX(leftDistance, leftAngle);
+		double z = playerPosition.zInOverworld() + Coords.getZ(leftDistance, leftAngle);
+		double horizontalAngle = Coords.getPhi(stronghold.eightEightX() - x, stronghold.eightEightZ() - z) / Math.PI * 180.0;
+		return new DetailedPlayerPosition(x, 80, z, horizontalAngle, -31, false);
+	}
 
-    private static int getFirstInt(String[] words) {
-        for (String word : words) {
-            try {
-                return Integer.parseInt(word);
-            } catch (NumberFormatException ignored) {
-            }
-        }
-        throw new NumberFormatException("Could not parse string array: " + Arrays.toString(words));
-    }
+	private static int getFirstInt(String[] words) {
+		for (String word : words) {
+			try {
+				return Integer.parseInt(word);
+			} catch (NumberFormatException ignored) {
+			}
+		}
+		throw new NumberFormatException("Could not parse string array: " + Arrays.toString(words));
+	}
 
 }
