eeb7a4fdb83b8ff84855a1de0f5ebe9c7acc397d
Filip Ryblad <firybl@gmail.com>
2024-10-14 21:31:35 +0200

    Update Main.java

--- src/main/java/ninjabrainbot/Main.java
+++ src/main/java/ninjabrainbot/Main.java
@@ -17,7 +17,7 @@ import ninjabrainbot.util.Profiler;
 
 public class Main {
 
-	public static final String VERSION = "1.5.1";
+	public static final String VERSION = "1.5.2-PRE1";
 
 	public static void main(String[] args) {
 		ErrorHandler errorHandler = new ErrorHandler();
